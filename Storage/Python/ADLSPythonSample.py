9eab2bbcf60a940ed38202df2d3b396d1ee7a812
Amrinder-Singh29 <72441450+Amrinder-Singh29@users.noreply.github.com>
2020-12-24 13:55:49 +0530

    Python Script to Connect to ADLS Gen2 Account using SDK and perform isting
    Python Script to Connect to ADLS Gen2 Account using SDK and perform isting

--- Storage/Python/ADLSPythonSample.py
+++ Storage/Python/ADLSPythonSample.py
@@ -5,7 +5,6 @@
 # The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
 # In no event shall Microsoft, its authors, owners of this repository or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including,
 # without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
-
 #-------------------------------------------------------------------------
 
 #IMPORT THE LIBRARIES INTO YOUR FILE
