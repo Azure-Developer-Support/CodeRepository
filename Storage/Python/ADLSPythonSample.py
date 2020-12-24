15f35bdfff0721185397107abf569cd63f92120a
Amrinder-Singh29 <72441450+Amrinder-Singh29@users.noreply.github.com>
2020-12-24 13:45:29 +0530

    Update ADLSPythonSample.py
    Adding Disclaimer

--- Storage/Python/ADLSPythonSample.py
+++ Storage/Python/ADLSPythonSample.py
@@ -1,7 +1,11 @@
 #-------------------------------------------------------------------------
-# The below script is a sample piece of code that helps you connecting to an ADLS Gen 2 account using Azure SDK for Python
-# There are differnet authenticaion mechanism for making the connection and I have made use of 2 of them 
-# Listing operation has been performed over the container.
+#DISCLAIMER
+#The sample scripts are not supported under any Microsoft standard support program or service.
+# The sample scripts are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
+# The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
+# In no event shall Microsoft, its authors, owners of this repository or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including,
+# without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
+
 #-------------------------------------------------------------------------
 
 #IMPORT THE LIBRARIES INTO YOUR FILE
