81ecffb638878de0a27ace53a7b2b37aa177c885
Himanshu Bohra <73924927+hibohra98@users.noreply.github.com>
2021-01-24 20:12:50 +0530

    Create Create-remediation-for-initiatives.ps1

---
+++ AzurePolicy/Powershell/Create-remediation-for-initiatives.ps1
@@ -0,0 +1,19 @@
+# DISCLAIMER #
+#The sample scripts are not supported under any Microsoft standard support program or service. 
+#The sample scripts are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. 
+
+##Create two variable Policy Assignment Id and PolicySetDefinitionId
+$policyAssignmentId = '<Add-Policy-Assignment-Id>'
+$policySetDefID = '<Add-Policy-Set-Definition-Id>'
+
+##Fetch the policy set definition properties
+$policySetDef = Get-AzPolicySetDefinition -Id $policySetDefID
+
+##Adding Policy Set Definitinos array to a variable 
+$definitions = $policySetDef.Properties.PolicyDefinitions
+
+##Add remediation for all the policy definition which are part of initiative definition
+Foreach ($def in $definitions){
+$rID = $def.policydefinitionReferenceId
+Start-AzPolicyRemediation -Name $rId -PolicyDefinitionReferenceId $rId -PolicyAssignmentId $policyAssignmentId 
+} 
