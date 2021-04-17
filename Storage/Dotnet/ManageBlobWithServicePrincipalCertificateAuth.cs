c60714342755de1a0825c9dd040526af8ef5b3ea
Ankit-MSFT <72929864+Ankit-MSFT@users.noreply.github.com>
2021-04-17 17:23:38 +0530

    Create ManageBlobWithServicePrincipalCertificateAuth.cs

---
+++ Storage/Dotnet/ManageBlobWithServicePrincipalCertificateAuth.cs
@@ -0,0 +1,58 @@
+﻿//'By using the following materials or sample code you agree to be bound by the license terms below 
+//'and the Microsoft Partner Program Agreement the terms of which are incorporated herein by this reference. 
+//'These license terms are an agreement between Microsoft Corporation (or, if applicable based on where you 
+//'are located, one of its affiliates) and you. Any materials (other than sample code) we provide to you 
+//'are for your internal use only. Any sample code is provided for the purpose of illustration only and is 
+//'not intended to be used in a production environment. We grant you a nonexclusive, royalty-free right to 
+//'use and modify the sample code and to reproduce and distribute the object code form of the sample code, 
+//'provided that you agree: (i) to not use Microsoft’s name, logo, or trademarks to market your software product 
+//'in which the sample code is embedded; (ii) to include a valid copyright notice on your software product in 
+//'which the sample code is embedded; (iii) to provide on behalf of and for the benefit of your subcontractors 
+//'a disclaimer of warranties, exclusion of liability for indirect and consequential damages and a reasonable 
+//'limitation of liability; and (iv) to indemnify, hold harmless, and defend Microsoft, its affiliates and 
+//'suppliers from and against any third party claims or lawsuits, including attorneys’ fees, that arise or result 
+//'from the use or distribution of the sample code."  
+
+// Dependencies: Following nuget packages needs to be installed
+// Azure.Storage.Blobs
+// Azure.Identity
+
+// This class file contains .Net Core 3.1 sample code to authenticate a service principal with certificate and use the certificate credentials to list blobs in a storage account.
+
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+
+namespace BlobSamples.SPNCertificates
+{
+    class ManageBlobWithServicePrincipalCertificateAuth
+    {
+        public void Main()
+        {
+            ClientCertificateCredential certificateCredential = new ClientCertificateCredential(
+                    "TenantID", "ClientID", "C:\\Path\\To\\Private\\Key.pem"
+            );
+
+			// Storage accounts blob service URI
+            Uri accountUri = new Uri("https://STORAGEACCOUNT.blob.core.windows.net/");
+
+            // Set the container name
+            string containerName = "testcontainer";
+
+            // Create blob service client using certificate credentials
+            BlobServiceClient blobService = new BlobServiceClient(accountUri, certificateCredential);
+
+			// Get a service client to the container
+            BlobContainerClient containerClient = blobService.GetBlobContainerClient(containerName);
+
+            // Get and print names of all the blobs
+            foreach (BlobItem blob in containerClient.GetBlobs())
+            {
+                Console.WriteLine(blob.Name);
+            }
+
+            Console.Read();
+        }
+    }
+}
