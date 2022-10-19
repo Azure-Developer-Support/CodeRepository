cc5471a6d41ac78ffa2bc784bff57983d353f7d7
sihegde <58765848+sihegde@users.noreply.github.com>
2022-10-19 12:59:52 +0530

    PS_DeleteBlobVersions_BasedOn_LastModified.ps1

---
+++ Storage/Powershell/PS_DeleteBlobVersions_BasedOn_LastModified.ps1
@@ -0,0 +1,111 @@
+#The below PowerShell script helps you to delete the older blob versions based on the Last Modified date of the versions.
+#This doesn't delete the Root blob/current version.
+#The script can be modified based on the requirements.
+
+#Disclaimer
+
+#By using the following materials or sample code you agree to be bound by the license terms below 
+#and the Microsoft Partner Program Agreement the terms of which are incorporated herein by this reference. 
+#These license terms are an agreement between Microsoft Corporation (or, if applicable based on where you 
+#are located, one of its affiliates) and you. Any materials (other than sample code) we provide to you 
+#are for your internal use only. Any sample code is provided for the purpose of illustration only and is 
+#not intended to be used in a production environment. We grant you a nonexclusive, royalty-free right to 
+#use and modify the sample code and to reproduce and distribute the object code form of the sample code, 
+#provided that you agree: (i) to not use Microsoftâ€™s name, logo, or trademarks to market your software product 
+#in which the sample code is embedded; (ii) to include a valid copyright notice on your software product in 
+#which the sample code is embedded; (iii) to provide on behalf of and for the benefit of your subcontractors 
+#a disclaimer of warranties, exclusion of liability for indirect and consequential damages and a reasonable 
+#limitation of liability; and (iv) to indemnify, hold harmless, and defend Microsoft, its affiliates and 
+#suppliers from and against any third party claims or lawsuits, including attorneysâ€™ fees, that arise or result 
+#from the use or distribution of the sample code."
+
+
+[CmdletBinding(DefaultParametersetName="SharedKey")]
+param(
+
+  [Parameter(Mandatory=$true, HelpMessage="Storage Account Name")] 
+  [String] $storage_account_name,
+
+  [Parameter(Mandatory=$true, HelpMessage="Any one of the two shared access keys", ParameterSetName="SharedKey", Position=1)] 
+  [String] $storage_shared_key,
+  
+  [Parameter(Mandatory=$true, HelpMessage="Container Name")] 
+  [String] $container,
+
+  [Parameter(Mandatory=$true, HelpMessage="Number of days you want to keep the versions")] 
+  [String] $number_of_days_old
+  
+)
+
+
+$current_date = get-date
+$date_older_for_version_to_be_deleted = $current_date.AddDays(-$number_of_days_old)
+
+$containerstats = @()
+
+  $Ctx = New-AzStorageContext -StorageAccountName $storage_account_name -StorageAccountKey $storage_shared_key
+
+      Write-Host "Container List"  $container
+
+      Write-Verbose "Processing container : $container"
+
+      $total_usage = 0
+      $total_blob_count = 0
+      $version_count =0 
+      $version_usage =0
+      $blob_continuation_token = $null
+    
+      $blobs = Get-AzStorageBlob -Context $Ctx -Container $container -MaxCount 5000 -IncludeVersion -IncludeDeleted -ContinuationToken $blob_continuation_token
+     
+      do {
+    
+                        
+        $blob_continuation_token = $null;
+
+        if ($blobs -ne $null)
+        {
+          $blob_continuation_token = $blobs[$blobs.Count - 1].ContinuationToken
+
+          for ([int] $b = 0; $b -lt $blobs.Count; $b++)
+          {
+            $total_blob_count++
+            $total_usage += $blobs[$b].Length
+            if ($blobs[$b].VersionId)
+            {
+              
+              if ($blobs[$b].LastModified -le $date_older_for_version_to_be_deleted)
+              {
+              $version_count++
+              $version_usage += $blobs[$b].Length
+              ##Removes all the older versions based on the number of days given
+                Remove-AzStorageBlob -Container $container -Blob $blobs[$b].Name -VersionId $blobs[$b].VersionId -Context  $Ctx
+              
+              }
+            }
+          }
+
+          If ($blob_continuation_token -ne $null)
+          {
+            Write-Verbose "Blob listing continuation token = {0}".Replace("{0}",$blob_continuation_token.NextMarker)
+          }
+        }
+     } while ($blob_continuation_token -ne $null)
+
+      Write-Verbose "Calculated size of $container = $total_usage with Usage of versions"
+                        
+      $containerstats += [PSCustomObject] @{ 
+        Name = $container 
+        TotalBlobCount = $total_blob_count 
+        TotalBlobUsage = $total_usage 
+        VersionCount = $version_count
+        VersionUsage = $version_usage
+      }
+
+
+Write-Host "Total container stats"
+$containerstats | Format-Table -AutoSize  
+
+Write-Host "YOU CAN IGNORE THE ABOVE EXCEPTIONS AS ITS JUST A CHECK ON ROOT BLOB and deleted version" -ForegroundColor DarkGreen -BackgroundColor Yellow
+
+Write-Host "Deleted the Blob versions succesfully. GO to the container in Azure Portal and check to confirm" -ForegroundColor DarkGreen -BackgroundColor Yellow
+
